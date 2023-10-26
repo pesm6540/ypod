7787a91dd50843c594d6c722c189d69a20d3dcb1
pesm6540 <percy.smith@colorado.edu>
2023-10-26 13:53:37 -0600

    YPODV3.0.5
    Fixes Green LED and PM sampling issue, reverts formatting to a previous version, to be re-updated to the formatting 3.0.0.

--- libraries/Adafruit_BusIO/Adafruit_SPIDevice.h
+++ libraries/Adafruit_BusIO/Adafruit_SPIDevice.h
@@ -22,8 +22,7 @@ typedef uint8_t SPIClass;
     defined(ARDUINO_AVR_ATmega4808) || defined(ARDUINO_AVR_ATmega3209) ||      \
     defined(ARDUINO_AVR_ATmega3208) || defined(ARDUINO_AVR_ATmega1609) ||      \
     defined(ARDUINO_AVR_ATmega1608) || defined(ARDUINO_AVR_ATmega809) ||       \
-    defined(ARDUINO_AVR_ATmega808) || defined(ARDUINO_ARCH_ARC32) ||           \
-    defined(ARDUINO_ARCH_XMC)
+    defined(ARDUINO_AVR_ATmega808) || defined(ARDUINO_ARCH_ARC32)
 
 typedef enum _BitOrder {
   SPI_BITORDER_MSBFIRST = MSBFIRST,
@@ -57,9 +56,6 @@ typedef BitOrder BusIOBitOrder;
 // typedef volatile uint32_t BusIO_PortReg;
 // typedef uint32_t BusIO_PortMask;
 //#define BUSIO_USE_FAST_PINIO
-
-#elif defined(ARDUINO_ARCH_XMC)
-#undef BUSIO_USE_FAST_PINIO
 
 #elif defined(__AVR__) || defined(TEENSYDUINO)
 typedef volatile uint8_t BusIO_PortReg;
